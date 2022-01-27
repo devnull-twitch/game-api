9481c2279f30c5896a16504c15bb4872ee7623d5
devnull-twitch <devnull@spindelhirn.me>
2022-01-27 19:14:29 +0100

    fix: port finder for k8s env

--- pkg/k8s/port_finder.go
+++ pkg/k8s/port_finder.go
@@ -3,32 +3,30 @@ package k8s
 import (
 	"context"
 	"fmt"
+	"strings"
 
+	"github.com/sirupsen/logrus"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
 func GetPortFinder(clientset *kubernetes.Clientset) func(string) int {
 	return func(zone string) int {
-		pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{
-			LabelSelector: "app=gameserver",
+		k8sZone := strings.ReplaceAll(zone, "_", "-")
+
+		services, err := clientset.CoreV1().Services("").List(context.Background(), metav1.ListOptions{
+			LabelSelector: fmt.Sprintf("gamezone=%s", k8sZone),
 		})
 		if err != nil {
+			logrus.WithError(err).Error("unable to load services")
 			return 0
 		}
 
-		for _, p := range pods.Items {
-			if p.GetLabels()["app"] == "gameserver" && p.GetLabels()["gamezone"] == zone {
-				services, err := clientset.CoreV1().Services("").List(context.Background(), metav1.ListOptions{
-					LabelSelector: fmt.Sprintf("gamezone=%s", zone),
-				})
-				if err != nil {
-					return 0
-				}
-				return int(services.Items[0].Spec.Ports[0].NodePort)
-			}
+		if len(services.Items) <= 0 {
+			logrus.WithError(err).Warn("no matching service")
+			return 0
 		}
 
-		return 0
+		return int(services.Items[0].Spec.Ports[0].NodePort)
 	}
 }
